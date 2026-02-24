1abbd01d64d06ebf1fcfec3e39c2636e8961da40
panyi <panyi@yixin.im>
2026-02-24 11:38:37 +0800

    add sum root to leaf

--- sum_root_to_leaf.cpp
+++ sum_root_to_leaf.cpp
@@ -1,60 +1,83 @@
-
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-
 struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-
-void travelBinaryTree(TreeNode *root ,int preSum, vector<int> &leafNodeValues){
-    if(root == nullptr){
-        return;
-    }
-
-    int sum = preSum << 1;
-    if(root->val == 1){
-        sum += 1;
-    }
-
-    if(isLeaf(root)){
-        leafNodeValues.push_back(sum);
-    }else{
-        travelBinaryTree(root->left , sum , leafNodeValues);
-        travelBinaryTree(root->right , sum , leafNodeValues);
-    }
-}
-
-bool isLeaf(TreeNode *node){
-    return node != nullptr && node->left == nullptr && node->right == nullptr;
-}
-
-int sumRootToLeaf(TreeNode* root) {
-    vector<int> leafNodeValues;
-    int sum = 0;
-
-    travelBinaryTree(root , sum , leafNodeValues);
-    
+int calBinVec(vector<int> bins){
     int result = 0;
-    for(int v : leafNodeValues){
-        //cout << v << "  ";
-        result += v;
-    }//end for each
-    cout << endl;
 
+    // for(auto &v : bins){
+    //     cout << v <<" ";
+    // }
+    // cout << std::endl;
+
+    for(int i = 0 ; i < bins.size() ; i++){
+        result = result << 1;
+
+        if(bins[i] == 1){
+            result += 1;
+        }
+    }//end for i
+
+    // cout << "sumvalue = " << result << std::endl;
     return result;
 }
 
-int main(){
-    return 0;
+void visitTree(TreeNode *node, vector<int> preVec, int &sumValue){
+    if(node == nullptr){
+        return;
+    }
+    
+    vector<int> result = preVec;
+
+    result.push_back(node->val);
+    if(node->left == nullptr && node->right == nullptr){
+        sumValue += calBinVec(result);
+    }else{
+        visitTree(node->left, result, sumValue);
+        visitTree(node->right, result, sumValue);
+    }
 }
 
+int sumRootToLeaf(TreeNode* root) {
+    int sumValue = 0;
+    vector<int> values;
+    visitTree(root, values, sumValue);
+    return sumValue;
+}
 
+int main(){
+    TreeNode root(1);
+    TreeNode n1(0);
+    TreeNode n2(1);
+    TreeNode n3(0);
+    TreeNode n4(1);
+    TreeNode n5(0);
+    TreeNode n6(1);
+
+    root.left = &n1;
+    root.right = &n2;
+
+    n1.left = &n3;
+    n1.right = &n4;
+
+    n2.left = &n5;
+    n2.right = &n6;
+
+    auto result = sumRootToLeaf(&root);
+    std::cout << "result = " << result << std::endl;
+
+
+    vector<int> data = {1,0,0};
+    std::cout << calBinVec(data) << std::endl;
+    return 0;
+}
